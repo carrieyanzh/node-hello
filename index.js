597fb9be6203a4ea35d8e26a747c897ce03f78a0
Hector Tornos <129948786+hector-tornos-NTT@users.noreply.github.com>
2025-04-01 13:21:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'testdb'
+});
+
+connection.connect();
+
+const userInput = "Redmond'; DROP TABLE OrdersTable; --";
+const query = `SELECT * FROM OrdersTable WHERE ShipCity = '${userInput}'`;
+
+connection.query(query, (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.end();
+
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
