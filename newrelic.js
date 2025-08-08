7c44329b8c797516d6f71e7e9d0523ff12aa5c72
Mariam El Madbouly <mariammadbouly49@gmail.com>
2025-08-08 23:38:15 +0300

    update docker file

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.config = {
-  app_name: ['Hello World App'],
+  app_name: ['Node Hello App'],
   license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     level: 'info',
